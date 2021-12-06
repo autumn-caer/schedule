94cc8acf0bb9364df32e036f3be2bdf99f9d2162
autumn-caer <muestra114@gmail.com>
2021-12-06 20:44:24 +0900

    section14 complete

--- web/frontend/src/practise03/bundler/index.ts
+++ web/frontend/src/practise03/bundler/index.ts
@@ -1,28 +1,35 @@
-import * as esbuild from 'esbuild-wasm';
-import { unpkgPathPlugin } from '../plugin/unpkg-path-plugin';
-import { fetchPlugin } from '../plugin/fetch-plugin';
+import * as esbuild from "esbuild-wasm";
+import { unpkgPathPlugin } from "../plugin/unpkg-path-plugin";
+import { fetchPlugin } from "../plugin/fetch-plugin";
 
 let service: esbuild.Service;
 const bundler = async (rawCode: string) => {
-    
-    if (!service) {
-        service =   await esbuild.startService({
-            worker: true,
-            wasmURL: 'https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm',
-        });
-    }
+  if (!service) {
+    service = await esbuild.startService({
+      worker: true,
+      wasmURL: "https://unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm",
+    });
+  }
+  try {
     const result = await service.build({
-        entryPoints: ['index.js'],
-        bundle: true,
-        write: false,
-        plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
-        define: {
-          'process.env.NODE_ENV': '"production"',
-          global: 'window',
-        },
-      });
+      entryPoints: ["index.js"],
+      bundle: true,
+      write: false,
+      plugins: [unpkgPathPlugin(), fetchPlugin(rawCode)],
+      define: {
+        "process.env.NODE_ENV": '"production"',
+        global: "window",
+      },
+    });
+    return { code: result.outputFiles[0].text, err: "" };
+  } catch (err) {
+    if (err instanceof Error) {
+      return { code: "", err: err.message };
+    } else {
+      return { code: "", err: '' };
+    }
+   
+  }
+};
 
-    return result.outputFiles[0].text
-}
-
-export default bundler
+export default bundler;
