00ce5de28fec1899ffed3bcc1d7393820ea90b59
autumn-caer <muestra114@gmail.com>
2021-12-28 11:54:11 +0900

    section19 complete

--- web/frontend/src/practise03/state/action-creators/index.ts
+++ web/frontend/src/practise03/state/action-creators/index.ts
@@ -1,3 +1,4 @@
+import { Dispatch } from "redux";
 import { ActionType } from "../action-types";
 import {
   Action,
@@ -5,9 +6,12 @@ import {
   DeleteCellAction,
   InsertCellAfterAction,
   UpdateCellAction,
-  Direction
+  BundleStartAction,
+  BundleCompleteAction,
+  Direction,
 } from "../actions";
 import { CellTypes } from "../cell";
+import bundle from "../../bundler";
 
 export const updateCell = (id: string, content: string): UpdateCellAction => {
   return {
@@ -25,10 +29,7 @@ export const deleteCell = (id: string): DeleteCellAction => {
   };
 };
 
-export const moveCell = (
-  id: string,
-  direction: Direction
-): MoveCellAction => {
+export const moveCell = (id: string, direction: Direction): MoveCellAction => {
   return {
     type: ActionType.MOVE_CELL,
     payload: {
@@ -48,5 +49,26 @@ export const insertCellAfter = (
       id,
       type: cellType,
     },
+  };
+};
+
+export const createBundle = (cellId: string, input: string) => {
+  return async (dispatch: Dispatch<Action>) => {
+    dispatch({
+      type: ActionType.BUNDLE_START,
+      payload: {
+        cellId: cellId,
+      },
+    });
+
+    const result = await bundle(input);
+
+    dispatch({
+      type: ActionType.BUNDLE_COMPLETE,
+      payload: {
+        cellId,
+        bundle: result,
+      },
+    });
   };
 };
